bd461334d24f99aed659703f55c3a78783723d81
Rendra Suroso <rs@cogsci.bandungfe.net>
2017-04-12 17:25:36 +0700

    Tweak build config.

--- src/PgSQL.php
+++ src/PgSQL.php
@@ -6,6 +6,14 @@ namespace BFITech\ZapStore;
 
 class PgSQL extends SQL {
 
+	/**
+	 * Constructor.
+	 *
+	 * @param array $params SQL connection dict exactly the same with
+	 *     that in the parent class except that 'dbtype' key can be
+	 *     omitted.
+	 * @param Logger $logger Instance of BFITech\\ZapCore\\Logger.
+	 */
 	public function __construct($params, $logger=null) {
 		$params['dbtype'] = 'pgsql';
 		parent::__construct($params, $logger);
