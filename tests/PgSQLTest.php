adadea5315e0152e70f043c7457c481eccc59b20
Rendra Suroso <rs@cogsci.bandungfe.net>
2019-05-10 00:44:16 +0700

    Remove useless includes on test.

--- tests/PgSQLTest.php
+++ tests/PgSQLTest.php
@@ -1,8 +1,6 @@
 <?php
 
 
-use BFITech\ZapCommonDev\CommonDev;
-
-
-require_once(__DIR__ . '/../vendor/bfitech/zapstore/tests/PgSQLTest.php');
+require_once dirname(__DIR__) .
+	'/vendor/bfitech/zapstore/tests/PgSQLTest.php';
 
